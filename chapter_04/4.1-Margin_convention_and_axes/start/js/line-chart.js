5438d14d648223ac0476c3c68f4324ff3dad2b49
T Z N <tuzunalican@gmail.com>
2024-11-01 13:11:35 +0100

    Update line-chart.js

--- chapter_04/4.1-Margin_convention_and_axes/start/js/line-chart.js
+++ chapter_04/4.1-Margin_convention_and_axes/start/js/line-chart.js
@@ -1,7 +1,8 @@
 // Load the data here
 d3.csv("../data/weekly_temperature.csv", d3.autoType).then(data => {
     console.log("temperature data", data);
-    });
+    drawLineChart(data);
+});
 
 // Create the line chart here
 const drawLineChart = (data) => {
@@ -10,27 +11,56 @@ const drawLineChart = (data) => {
     const height = 500;
     const innerWidth = width - margin.left - margin.bottom;
     const innerHeight = height - margin.top - margin.bottom;
+
     const svg = d3.select("#line-chart")
         .append("svg")
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .attr(stroke, "black")
-        .attr(stroke-width,3)
+        .attr("viewBox", `0, 0, ${width}, ${height}`)
+
     const innerChart = svg
         .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
-    const firstDate = d3.min(data, d => d.date);
+
+    const firstDate = new Date(2021, 00, 01, 0, 0, 0);
     const lastDate = d3.max(data, d => d.date);
     const xScale = d3.scaleTime()
         .domain([firstDate, lastDate])
         .range([0, innerWidth]);
+
     const maxTemp = d3.max(data, d => d.max_temp_F);
     const yScale = d3.scaleLinear()
         .domain([0, maxTemp])
         .range([innerHeight, 0]);
-    const bottomAxis = d3.axisBottom(xScale);
+
+    const bottomAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.timeFormat("%b"));
 
     innerChart
         .append("g")
-            .attr(":class", "axis-x")
-            .call(bottomAxis)
+        .attr("class", "axis-x")
+        .attr("transform", `translate(0, ${innerHeight})`)
+        .call(bottomAxis);
+
+    d3.selectAll(".axis-x text")
+        .attr("x", d => {
+            const currentMonth = d;
+            const nextMonth = new Date(2021, currentMonth.getMonth() + 1, 1);
+            return (xScale(nextMonth) - xScale(currentMonth)) / 2;
+        })
+        .attr("y", "10px")
+        .style("font-family", "Roboto, sans-serif")
+        .style("font-size", "14px");
+    const leftAxis = d3.axisLeft(yScale);
+    innerChart
+        .append("g")
+        .attr("class", "axis-y")
+        .call(leftAxis);
+    d3.selectAll(".axis-y text")
+        .attr("x", "-5px")
+        .style("font-family", "Roboto, sans-serif")
+        .style("font-size", "14px");
+
+    svg
+        .append("text")
+        .text("Temperature (°F)")
+        .attr("y", 20);
 };
